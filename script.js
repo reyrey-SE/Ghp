96c1bc986cb85cbcbf39d2055d15fe4353717e66
reyrey-SE <reynierdinglasan07@gmail.com>
2025-01-17 09:47:50 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('ctaButton');
+    button.addEventListener('click', () => {
+        alert('Thank you for visiting my GitHub Page!');
+    });
+});
